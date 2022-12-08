19e81e2de59af1dea87cbcfa71048c1da97b9ba4
victorgabriel <112019336+VictorGabriel01@users.noreply.github.com>
2022-12-08 01:10:31 -0300

    Add files via upload

---
+++ Exercicio javascript lista 03/js/script2.js
@@ -0,0 +1,7 @@
+var comprimento = parseFloat(prompt("Digite o comprimento"));
+var largura = parseFloat(prompt("Digite a largura"));
+var altura = parseFloat(prompt("Digite a altura"));
+var volume = comprimento*largura*altura;
+alert("O volume da caixa é = "+volume);
+
+
---
+++ Exercicio javascript lista 03/js/script3.js
@@ -0,0 +1,5 @@
+var A = parseInt(prompt("Digite um número"));
+var B = parseInt(prompt("Digite um número"));
+var Difevalor = A-B ; 
+var resultado = A**2;
+alert("O resultado da operação é = "+resultado);
